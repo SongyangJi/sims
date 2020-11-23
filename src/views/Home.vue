1d50b44212917a4c6484b6bdb51fbef5878f967f
SongyangJi <1539242799@qq.com>
2020-11-23 19:32:15 +0800

    jsy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <el-container class="home_container">
+
+        <!-- 顶部导航 -->
+        <el-header class="head"><HeadBar></HeadBar></el-header>
+
+        <el-container>
+            <!-- 侧边栏导航 -->
+            <el-aside><SiderBar></SiderBar></el-aside>
+
+            <!-- 中间主界面 -->
+
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+
+
+        </el-container>
+
+
+        <el-footer><BottomBar></BottomBar></el-footer>
+
+
+    </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    import SiderBar from "../components/SiderBar";
+    import HeadBar from "../components/HeadBar";
+    import BottomBar from "../components/BottomBar";
+    export default {
+        name: "Home",
+        components:{
+            SiderBar,
+            HeadBar,
+            BottomBar,
+        }
+    }
 </script>
+
+<style scoped>
+
+    .home_container {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+    }
+
+
+    .head {
+        display: flex;
+        align-items: center;
+    }
+
+
+</style>
